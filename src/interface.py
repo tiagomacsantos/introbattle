681d5b93c3883fc84cda7354b1b4934a1b58a509
tiagomacsantos <tiagomachado909@gmail.com>
2024-08-13 20:19:19 -0300

    alteracoes nos nomes de arquivos

--- src/interface.py
+++ src/interface.py
@@ -16,40 +16,64 @@ class Window():
     def get_color(self):
         return self.color
     
-# FALTA DESENHAR OS QUADRADOS DOS PERSONAGENS E OS PERSONAGENS
+# FALTA DESENHAR OS PERSONAGENS
 def desenha_menu(janela):
     #Titulo do introbattle
     fonte = pygame.font.Font('freesansbold.ttf', 50) #Declaracao da fonte
     texto = fonte.render("IntroBattle", True, (255, 255, 255)) #Renderiza texto
     rect_introbattle = pygame.Rect((janela.get_width() / 2 - 200, 60), (400, 90)) #Criacao do retangulo
-    pygame.draw.rect(janela, (0, 0, 0), rect_introbattle) #Retangulo do titulo
-    pygame.draw.rect(janela, (255, 255, 255), rect_introbattle, 7) #Borda
     texto_rect = texto.get_rect(center=rect_introbattle.center) #Acha o centro do retangulo
     
     #Personagens
-    personagem_tamanho = (400, 400)
-    personagens = ["paladin", "rougue", "wizard", "hunter", "priest"]
+    personagem_tamanho = (150, 150)
+    
+    paladin = pygame.image.load("./imgs/personagens/paladin.png")
+    rogue = pygame.image.load("./imgs/personagens/rogue.png")
+    priest = pygame.image.load("./imgs/personagens/priest.png")         #Inicia as variaveis que vao guardar as imagens
+    hunter = pygame.image.load("./imgs/personagens/hunter.png")
+    wizard = pygame.image.load("./imgs/personagens/wizard.png")
+    wizard = pygame.transform.flip(wizard, True, False) #invertendo horizontalmente a imagem do bruxo
+    
+    personagens = [
+        paladin,
+        rogue,
+        wizard,         #Salva as imagens em uma lista
+        hunter,
+        priest,
+    ]
+
+    for i in range(len(personagens)):
+        personagens[i] = pygame.transform.scale(personagens[i], personagem_tamanho) #Redimensiona o tamanho dos sprites dos personagens
+    
+    personagem_posicoes = [
+        (137, 235), #paladin
+        (432, 235), #rogue
+        (720, 235), #wizard                  
+        (290, 485), #hunter
+        (576, 485), #priest
+    ]
 
 
-    #Quadrado personagem
-    botao_tamanho = (500, 500)
+    #Quadrado do personagem
+    botao_tamanho = (550, 500)
     botao_personagem = pygame.image.load("./imgs/UI/introcomp_menu.png")
     botao_personagem = pygame.transform.scale(botao_personagem, botao_tamanho)
     botao_posicoes = [
-        (150, 150),
-        (437, 150),
-        (724, 150),
-        (287, 400),
-        (574, 400),
+        (-200, -50),
+        (87, -50),
+        (374, -50),
+        (-53, 200),
+        (234, 200),
     ]
 
-    for i in range(5):
-        janela.blit(botao_personagem, botao_posicoes[i])
     
     #desenhos na tela
+    pygame.draw.rect(janela, (0, 0, 0), rect_introbattle) #Retangulo do titulo
+    pygame.draw.rect(janela, (255, 255, 255), rect_introbattle, 7) #Borda do retangulo do titulo
     janela.blit(texto, texto_rect) #Desenha o texto na tela
+    for i in range(5):
+        janela.blit(botao_personagem, botao_posicoes[i]) #Desenha os botoes
+        janela.blit(personagens[i], personagem_posicoes[i]) #Desenha os personagens
+    
 
     return True
-
-
-        
